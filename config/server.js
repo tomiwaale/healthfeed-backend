2b65a55889bf8ef46a233a6b62d611d715c2937e
Tomiwa <aletomiwa@gmail.com>
2021-02-12 04:08:21 +0100

    changed file

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0c812ded6009e5aebbf70646b804913c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c7904950-0686-40c7-8f08-f22bce5e0a49'
+};
